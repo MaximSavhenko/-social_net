ad50f14afd293274cf6e9fba45de3a2b3b9f4c16
Maxim Savchenko <maxim.savchenko@wdg.com.ua>
2021-07-04 14:57:24 +0300

    add routers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import './App.scss';
-import Profile from './components/Profile/Profile';
-import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
-import Dialogs from './components/Dialogs/Dialogs';
+import React from 'react'
+import './App.scss'
+import Profile from './components/Profile/Profile'
+import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar'
+import Dialogs from './components/Dialogs/Dialogs'
+import News from './components/News/News'
+import Music from './components/Music/Music'
+import Settings from './components/Settings/Settings'
+import {BrowserRouter , Route} from 'react-router-dom'
 
 const App= () => {
   return (
-    <div className='wrapper'>
-      <Header/>
-      <Sidebar/>
-      <div className='app-wrapper-content'>
-        <Dialogs/>
+    <BrowserRouter>
+      <div className='wrapper'>
+        <Header/>
+        <Sidebar/>
+        <div className='app-wrapper-content'>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/messages' component={Dialogs}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
+        </div>
       </div>
-      {/* <Profile/> */}
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Sidebar.module.scss'
 
 const Sidebar = () => {
     return (
         <nav className={s.sidebar}>
             <ul className={s.menu}>
-            <li className={s.item}><a href='#'>Profile</a></li>
-            <li className={s.item}><a href='#'>Messages</a></li>
-            <li className={s.item}><a href='#'>News</a></li>
-            <li className={s.item}><a href='#'>Music</a></li>
+            <li className={s.item}><NavLink to='/profile' activeClassName={s.active}>Profile</NavLink></li>
+            <li className={s.item}><NavLink to='/messages' activeClassName={s.active}>Messages</NavLink></li>
+            <li className={s.item}><NavLink to='/news' activeClassName={s.active}>News</NavLink></li>
+            <li className={s.item}><NavLink to='/music' activeClassName={s.active}>Music</NavLink></li>
             </ul>
             <ul className={s.settings}>
-            <li className={s.item}><a href='#'>Settings</a></li>
+            <li className={s.item}><NavLink to='/settings' activeClassName={s.active}>Settings</NavLink></li>
             </ul>
         </nav>
     )
